5d93736dbef80f1cc387b8d395cf00ab4df817bf
rina <rina@rinanoMacBook-puro.local>
2019-03-30 18:45:31 +0900

    [finish] 8-9④

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,53 +1,62 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :show, :index ]
 
-  def new
-  	@book = Book.new
-  end
+	def new
+		@book = Book.new
+	end
 
-  def create
-  	book = Book.new(book_params)
-  	book.user_id = current_user.id
-  	if book.save
-  	flash[:notice] = "successfully"
-  	redirect_to book_path(book.id)
-  else
-  	@book = book
-  	@books = Book.all
-  	@user  = current_user
-  	flash[:notice] ="error"
-  	render :index
-  end
-  end
+	def create
+		book = Book.new(book_params)
+		book.user_id = current_user.id
+		if book.save
+			flash[:notice] = "successfully"
+			redirect_to book_path(book.id)
+		else
+			@book = book
+			@books = Book.all
+			@user  = current_user
+			flash[:notice] ="error"
+			render :index
+		end
+	end
 
-  def show
-    @book = Book.find(params[:id])
-  end
+	def show
+		@book = Book.find(params[:id])
+		@user = User.find(@book.user_id)
+	end
 
-  def index
-  	@book = Book.new
-  	@books = Book.all
-  	@user  = current_user
-  end
+	def index
+		@book = Book.new
+		@books = Book.all
+		@user  = current_user
+	end
 
-  def edit
-  	@book = Book.find(params[:id])
-  end
+	def edit
+		@book = Book.find(params[:id])
+		
+	end
 
-    def update
-      book = Book.find(params[:id])
-  	    if book.update(book_params)
-		  redirect_to book_path(book.id)
-	      flash[:notice] = "successfully"
-	    else
-	    @book= book
-  	    flash[:notice] = "error"
-  	    render :edit
-   end
-end
+	def update
+		book = Book.find(params[:id])
+		if book.update(book_params)
+			redirect_to book_path(book.id)
+			flash[:notice] = "successfully"
+		else
+			@book= book
+			flash[:notice] = "error"
+			render :edit
+		end
+	end
 
-  private
+		def destroy
+			@book = Book.find(params[:id])
+			@book.destroy
+			redirect_to books_path
+		end
 
-    def book_params
-        params.require(:book).permit(:title, :opinion)
-    end
+	private
+
+	def book_params
+		params.require(:book).permit(:title, :opinion)
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, only: [:new, :show, :index ]
+
   def show
   	@user = User.find(params[:id])
   	@book = Book.new
@@ -13,8 +14,8 @@ class UsersController < ApplicationController
 
   def edit
   	@user = User.find(params[:id])
-  	if user.id != current_user.id
-  		redirect_to user_path(@user)
+  	if @user.id != current_user.id
+  		redirect_to user_path(current_user)
   	end
   end
 
